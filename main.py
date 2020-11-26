c0eda9c69bb7b987283b27f6c1f1e2b795e268b7
Timothy Wong <timwong101@gmail.com>
2020-11-26 16:27:37 -0500

    initialSeeds

--- main.py
+++ main.py
@@ -15,6 +15,7 @@ from pyspark.sql.functions import row_number, monotonically_increasing_id
 from pyspark.sql import Window
 import pandas as pd
 import numpy as np
+import math
 
 
 class DisplayRDD:
@@ -32,52 +33,46 @@ class DisplayRDD:
 def dist(row):
   vector = []
   row_df = row.to_frame()
-  """
-  print("row_df type: \n", type(row_df))
-  print("row_df: \n", row_df)
-  print("row_df index: \n", row_df.index)
-  print("row_df values shape: \n", row_df.values.reshape(-1, 1).shape)
-  print("row_df shape: \n", row_df.shape)
-  print("row_df transpose: \n", row_df.transpose())
-  """
   row_df = row_df.transpose()
   for m in medoidsList:
-    row_df_sum = row_df.to_numpy().sum()
-    m_sum = m.to_numpy().sum()
-    sum = row_df_sum - m_sum
-    sum = sum**2
-    print("row_df: \n", row_df)
-    print("row_df_sum: ", row_df_sum)
-    print("m_sum: ", m_sum)
-    print("sum: ", sum)
+    sum = 0
+    for i in range(0, len(m.values[0])):
+        m_value = m.values[0][i]
+        row_df_value = row_df.values[0][i]
+        sum += math.sqrt((m_value - row_df_value)**2)
     vector.append(sum)
   print("vector: ", vector)
-  return vector
+  return min(vector)
 
 def initialSeeds(df, k):
+  N = df.count()
   dfp = df.toPandas()
   sample = dfp.sample()
   global medoidsList
   medoidsList = []
   medoidsList.append(sample)
 
-  distances_series = dfp.apply(dist, axis = 1)
-  distancesdf = distances_series.to_frame()
-  return distancesdf
+  for iter in range(1,k):
+    distances_series = dfp.apply(dist, axis = 1)
+    distancesdf = distances_series.to_frame()
+    distances_array = np.concatenate(distancesdf.values, axis = 0)
+    sum = distancesdf[0].sum()
+    #distances_list = sqlContext.createDataFrame(distancesdf).collect()
+    distances_array = [distance / sum for distance in distances_array]
+    newMedoidId = np.random.choice(N, 1, p = distances_array)
+    newMedoid = dfp.iloc[newMedoidId[0]]
+    newMedoid = newMedoid.to_frame().transpose()
+    medoidsList.append(newMedoid)
+  return medoidsList
 
 
-
-
-#print(type(medoids[0]), str(medoids[0]))
-#DisplayRDD(medoids)
-#print("Type: ", (medoids.head()))
-#print(medoids)
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     conf = SparkConf().setAppName("project-gam")
     sc = SparkContext(conf=conf)
     sqlContext = SQLContext(sc)
+    #spark = SparkSession.builder.getOrCreate()
     df = sqlContext.read.format('com.databricks.spark.csv').options(header='true', inferschema='true').load(
         "mushroom-attributions-200-samples.csv")
 
