5c07d869ad76cd700eb194df1beb8bded719d5f2
Timothy Wong <timwong101@gmail.com>
2020-12-02 14:42:53 -0800

    radar in main

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ from multiprocessing import  Pool
 from pandas.plotting import parallel_coordinates
 import matplotlib.pyplot as plt
 from matplotlib.cm import get_cmap
+from math import pi
 
 
 
@@ -181,9 +182,63 @@ if __name__ == '__main__':
     for i in list(groupsDict.keys()):
         groupsList.append(groupsDict[i])
     print(groupsDict)
+    # adding medoid column for plotting purposes
     dfp['medoid'] = 0
     for key, value in groupsDict.items():
         dfp.loc[value, 'medoid'] = key
-    parallel_coordinates(dfp, class_column='medoid', colormap=get_cmap("Set1"))
-    plt.show()
 
+    graph = 'faceted radar'
+    if graph == 'parallel':
+        parallel_coordinates(dfp, class_column='medoid', colormap=get_cmap("Set1"))
+        plt.show()
+    elif graph == 'radar':
+        # create background
+        col = dfp.pop('medoid')
+        dfp.insert(0, 'medoid', col)
+        categories = list(dfp)[1:]
+        N = len(categories)
+        angles = [n / float(N) * 2 * pi for n in range(N)]
+        angles += angles[:1]
+        ax = plt.subplot(111, polar=True)
+        ax.set_theta_offset(pi / 2)
+        ax.set_theta_direction(-1)
+        plt.xticks(angles[:-1], categories)
+        ax.set_rlabel_position(0)
+        plt.yticks([.1, .2, .3], [".1", ".2", ".3"], color="grey", size=7)
+        plt.ylim(0, .3)
+        # add plots
+        for i in range(len(medoidsList)):
+            medoidname = medoidsList[i].index[0]
+            values = medoidsList[i].values.flatten().tolist()
+            values += values[:1]
+            ax.plot(angles, values, linewidth=1, linestyle='solid', label=medoidname)
+            ax.fill(angles, values, 'b', alpha=0.1)
+        plt.legend(loc='upper right', bbox_to_anchor=(0.1, 0.1))
+        plt.show()
+    elif graph == 'faceted radar':
+        def make_spider(row, title, color):
+            col = dfp.pop('medoid')
+            dfp.insert(0, 'medoid', col)
+            categories = list(dfp)[1:]
+            N = len(categories)
+            angles = [n / float(N) * 2 * pi for n in range(N)]
+            angles += angles[:1]
+            ax = plt.subplot(2, 2, row + 1, polar=True, )
+            ax.set_theta_offset(pi / 2)
+            ax.set_theta_direction(-1)
+            plt.xticks(angles[:-1], categories, color='grey', size=8)
+            ax.set_rlabel_position(0)
+            plt.yticks([.1, .2, .3], [".1", ".2", ".3"], color="grey", size=7)
+            plt.ylim(0, .3)
+            medoidname = medoidsList[row].index[0]
+            values = medoidsList[row].values.flatten().tolist()
+            values += values[:1]
+            ax.plot(angles, values, linewidth=1, linestyle='solid', label=medoidname)
+            ax.fill(angles, values, 'b', alpha=0.1)
+            plt.title(title, size = 11, color = color, y = 1.1)
+        my_dpi = 96
+        plt.figure(figsize=(1000 / my_dpi, 1000 / my_dpi), dpi=my_dpi)
+        my_palette = get_cmap("Set2", len(medoidsList))
+        for row in range(len(medoidsList)):
+            make_spider(row = row, title = medoidsList[row].index[0], color = my_palette(row))
+        plt.show()
