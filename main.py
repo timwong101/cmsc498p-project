c77ad0aedea727ee9d08eca4cb8c78f5a23e2a42
sonofmammon <sonofmammon@gmail.com>
2020-12-17 00:42:06 -0500

    'start'

--- main.py
+++ main.py
@@ -121,122 +121,129 @@ def parallelize_dataframe(df, func, n_cores=4):
     return df
 """
 if __name__ == '__main__':
-    # conf = SparkConf().setAppName("project-gam")
-    # sc = SparkContext(conf=conf)
-    sc = SparkContext()
-    # Set new loglevel: "ALL", "DEBUG", "ERROR", "FATAL", "INFO", "OFF", "TRACE", "WARN"
-    sc.setLogLevel("OFF")
-    sqlContext = SQLContext(sc)
-    # sets up the initial df and initializes variables
-    df = sqlContext.read.format('com.databricks.spark.csv').options(header='true', inferschema='true').load(
-        "mushroom-attributions-200-samples.csv")
-    max_iter = 10; k = 3; WCSoD1 = float("inf")
-    approximateTrackingFlag = False
-    medoids = initialSeeds(df, k)
-    global nearestClustersGlobal
-    dfp = df.toPandas()
-    # updates medoids until criteria is reached or the maximum number of iterations is reached
-    for iter in range(0,max_iter):
-        previousMedoids = copy.deepcopy(medoids)
 
-        distances_series  = apply_by_multiprocessing(dfp, distance, axis=1, workers=4)
-        #distances_series = dfp.apply(distance, axis=1)  # distancesRDD = datasetRDD.map(d(pattern,medoids)
-        nearestDistancesPDF = distances_series.to_frame()  # nearestRDD = distancesRDD.map(min(distanceVector))
+    import urllib.request as urllib
+    url = "https://archive.ics.uci.edu/ml/machine-learning-databases/communities/communities.data"
+    raw_data = urllib.urlopen(url)
+    with open('communities.csv', 'wb') as file:
+        file.write(raw_data.read())
 
-        nearest_clusters = apply_by_multiprocessing(dfp, nearestCluster, axis=1, workers=4)
-        #nearest_clusters = dfp.apply(nearestCluster, axis=1)  # nearestClusterRDD = distancesRDD.map(argmin(distanceVector));
-        nearestClustersPDF = nearest_clusters.to_frame()
-        nearestClustersGlobal = nearestClustersPDF
-        # updates each medoid for the given cluster
-        for mindex, m in enumerate(medoids):
-            clID = m.index[0]
-            patternsIndex = []
-            nearestClustersPDF_flat = nearestClustersPDF.values.flatten()
-            for i in range(len(nearestClustersPDF_flat)):
-                if nearestClustersPDF_flat[i] == clID:
-                    patternsIndex.append(i)
-            patternsInClusterPDF = dfp.iloc[patternsIndex,:]
-            newMedoid = exactMedoidUpdate(patternsInClusterPDF)
-            medoids[mindex] = newMedoid
-        WCSoD2 = nearestDistancesPDF.sum(axis = 0)[0]
-        if abs(WCSoD1 - WCSoD2) == 0:
-            medoids = previousMedoids
-            break
-        else:
-            WCSoD1 = WCSoD2
-
-    # groupsDict is a dictionary with the (key:value) as (medoid:list of samples in cluster)
-    groupsDict = {}
-    for i in range(len(medoidsList)):
-        groupsDict[medoidsList[i].index[0]] = nearestClustersGlobal[nearestClustersGlobal[0] == medoidsList[i].index[0]].index.tolist()
-    groupsList = []
-    for i in list(groupsDict.keys()):
-        groupsList.append(groupsDict[i])
-    print(groupsDict)
-    print(medoidsList)
-    # adding medoid column for plotting purposes
-    dfp['medoid'] = 0
-    for key, value in groupsDict.items():
-        dfp.loc[value, 'medoid'] = key
-
-    graph = 'silhouette'
-    # parallel plot of all points
-    if graph == 'parallel':
-        parallel_coordinates(dfp, class_column='medoid', colormap=get_cmap("Set1"))
-        plt.show()
-    # radar plot of medoids
-    elif graph == 'radar':
-        # create background
-        col = dfp.pop('medoid')
-        dfp.insert(0, 'medoid', col)
-        categories = list(dfp)[1:]
-        N = len(categories)
-        angles = [n / float(N) * 2 * pi for n in range(N)]
-        angles += angles[:1]
-        ax = plt.subplot(111, polar=True)
-        ax.set_theta_offset(pi / 2)
-        ax.set_theta_direction(-1)
-        plt.xticks(angles[:-1], categories)
-        ax.set_rlabel_position(0)
-        plt.yticks([.1, .2, .3], [".1", ".2", ".3"], color="grey", size=7)
-        plt.ylim(0, .3)
-        # add plots
-        for i in range(len(medoidsList)):
-            medoidname = medoidsList[i].index[0]
-            values = medoidsList[i].values.flatten().tolist()
-            values += values[:1]
-            ax.plot(angles, values, linewidth=1, linestyle='solid', label=medoidname)
-            ax.fill(angles, values, 'b', alpha=0.1)
-        plt.legend(loc='upper right', bbox_to_anchor=(0.1, 0.1))
-        plt.show()
-    # faceted radar plot of medoids
-    elif graph == 'faceted radar':
-        def make_spider(row, title, color):
-            col = dfp.pop('medoid')
-            dfp.insert(0, 'medoid', col)
-            categories = list(dfp)[1:]
-            N = len(categories)
-            angles = [n / float(N) * 2 * pi for n in range(N)]
-            angles += angles[:1]
-            ax = plt.subplot(2, 2, row + 1, polar=True, )
-            ax.set_theta_offset(pi / 2)
-            ax.set_theta_direction(-1)
-            plt.xticks(angles[:-1], categories, color='grey', size=8)
-            ax.set_rlabel_position(0)
-            plt.yticks([.1, .2, .3], [".1", ".2", ".3"], color="grey", size=7)
-            plt.ylim(0, .3)
-            medoidname = medoidsList[row].index[0]
-            values = medoidsList[row].values.flatten().tolist()
-            values += values[:1]
-            ax.plot(angles, values, linewidth=1, linestyle='solid', label=medoidname)
-            ax.fill(angles, values, 'b', alpha=0.1)
-            plt.title(title, size = 11, color = color, y = 1.1)
-        my_dpi = 96
-        plt.figure(figsize=(1000 / my_dpi, 1000 / my_dpi), dpi=my_dpi)
-        my_palette = get_cmap("Set2", len(medoidsList))
-        for row in range(len(medoidsList)):
-            make_spider(row = row, title = medoidsList[row].index[0], color = my_palette(row))
-        plt.show()
-    elif graph == 'silhouette':
-        print("to be implemented")
+    # # conf = SparkConf().setAppName("project-gam")
+    # # sc = SparkContext(conf=conf)
+    # sc = SparkContext()
+    # # Set new loglevel: "ALL", "DEBUG", "ERROR", "FATAL", "INFO", "OFF", "TRACE", "WARN"
+    # sc.setLogLevel("OFF")
+    # sqlContext = SQLContext(sc)
+    # # sets up the initial df and initializes variables
+    # df = sqlContext.read.format('com.databricks.spark.csv').options(header='true', inferschema='true').load(
+    #     "mushroom-attributions-200-samples.csv")
+    # max_iter = 10; k = 3; WCSoD1 = float("inf")
+    # approximateTrackingFlag = False
+    # medoids = initialSeeds(df, k)
+    # global nearestClustersGlobal
+    # dfp = df.toPandas()
+    # # updates medoids until criteria is reached or the maximum number of iterations is reached
+    # for iter in range(0,max_iter):
+    #     previousMedoids = copy.deepcopy(medoids)
+    #
+    #     distances_series  = apply_by_multiprocessing(dfp, distance, axis=1, workers=4)
+    #     #distances_series = dfp.apply(distance, axis=1)  # distancesRDD = datasetRDD.map(d(pattern,medoids)
+    #     nearestDistancesPDF = distances_series.to_frame()  # nearestRDD = distancesRDD.map(min(distanceVector))
+    #
+    #     nearest_clusters = apply_by_multiprocessing(dfp, nearestCluster, axis=1, workers=4)
+    #     #nearest_clusters = dfp.apply(nearestCluster, axis=1)  # nearestClusterRDD = distancesRDD.map(argmin(distanceVector));
+    #     nearestClustersPDF = nearest_clusters.to_frame()
+    #     nearestClustersGlobal = nearestClustersPDF
+    #     # updates each medoid for the given cluster
+    #     for mindex, m in enumerate(medoids):
+    #         clID = m.index[0]
+    #         patternsIndex = []
+    #         nearestClustersPDF_flat = nearestClustersPDF.values.flatten()
+    #         for i in range(len(nearestClustersPDF_flat)):
+    #             if nearestClustersPDF_flat[i] == clID:
+    #                 patternsIndex.append(i)
+    #         patternsInClusterPDF = dfp.iloc[patternsIndex,:]
+    #         newMedoid = exactMedoidUpdate(patternsInClusterPDF)
+    #         medoids[mindex] = newMedoid
+    #     WCSoD2 = nearestDistancesPDF.sum(axis = 0)[0]
+    #     if abs(WCSoD1 - WCSoD2) == 0:
+    #         medoids = previousMedoids
+    #         break
+    #     else:
+    #         WCSoD1 = WCSoD2
+    #
+    # # groupsDict is a dictionary with the (key:value) as (medoid:list of samples in cluster)
+    # groupsDict = {}
+    # for i in range(len(medoidsList)):
+    #     groupsDict[medoidsList[i].index[0]] = nearestClustersGlobal[nearestClustersGlobal[0] == medoidsList[i].index[0]].index.tolist()
+    # groupsList = []
+    # for i in list(groupsDict.keys()):
+    #     groupsList.append(groupsDict[i])
+    # print(groupsDict)
+    # print(medoidsList)
+    # # adding medoid column for plotting purposes
+    # dfp['medoid'] = 0
+    # for key, value in groupsDict.items():
+    #     dfp.loc[value, 'medoid'] = key
+    #
+    # graph = 'silhouette'
+    # # parallel plot of all points
+    # if graph == 'parallel':
+    #     parallel_coordinates(dfp, class_column='medoid', colormap=get_cmap("Set1"))
+    #     plt.show()
+    # # radar plot of medoids
+    # elif graph == 'radar':
+    #     # create background
+    #     col = dfp.pop('medoid')
+    #     dfp.insert(0, 'medoid', col)
+    #     categories = list(dfp)[1:]
+    #     N = len(categories)
+    #     angles = [n / float(N) * 2 * pi for n in range(N)]
+    #     angles += angles[:1]
+    #     ax = plt.subplot(111, polar=True)
+    #     ax.set_theta_offset(pi / 2)
+    #     ax.set_theta_direction(-1)
+    #     plt.xticks(angles[:-1], categories)
+    #     ax.set_rlabel_position(0)
+    #     plt.yticks([.1, .2, .3], [".1", ".2", ".3"], color="grey", size=7)
+    #     plt.ylim(0, .3)
+    #     # add plots
+    #     for i in range(len(medoidsList)):
+    #         medoidname = medoidsList[i].index[0]
+    #         values = medoidsList[i].values.flatten().tolist()
+    #         values += values[:1]
+    #         ax.plot(angles, values, linewidth=1, linestyle='solid', label=medoidname)
+    #         ax.fill(angles, values, 'b', alpha=0.1)
+    #     plt.legend(loc='upper right', bbox_to_anchor=(0.1, 0.1))
+    #     plt.show()
+    # # faceted radar plot of medoids
+    # elif graph == 'faceted radar':
+    #     def make_spider(row, title, color):
+    #         col = dfp.pop('medoid')
+    #         dfp.insert(0, 'medoid', col)
+    #         categories = list(dfp)[1:]
+    #         N = len(categories)
+    #         angles = [n / float(N) * 2 * pi for n in range(N)]
+    #         angles += angles[:1]
+    #         ax = plt.subplot(2, 2, row + 1, polar=True, )
+    #         ax.set_theta_offset(pi / 2)
+    #         ax.set_theta_direction(-1)
+    #         plt.xticks(angles[:-1], categories, color='grey', size=8)
+    #         ax.set_rlabel_position(0)
+    #         plt.yticks([.1, .2, .3], [".1", ".2", ".3"], color="grey", size=7)
+    #         plt.ylim(0, .3)
+    #         medoidname = medoidsList[row].index[0]
+    #         values = medoidsList[row].values.flatten().tolist()
+    #         values += values[:1]
+    #         ax.plot(angles, values, linewidth=1, linestyle='solid', label=medoidname)
+    #         ax.fill(angles, values, 'b', alpha=0.1)
+    #         plt.title(title, size = 11, color = color, y = 1.1)
+    #     my_dpi = 96
+    #     plt.figure(figsize=(1000 / my_dpi, 1000 / my_dpi), dpi=my_dpi)
+    #     my_palette = get_cmap("Set2", len(medoidsList))
+    #     for row in range(len(medoidsList)):
+    #         make_spider(row = row, title = medoidsList[row].index[0], color = my_palette(row))
+    #     plt.show()
+    # elif graph == 'silhouette':
+    #     print("to be implemented")
 
