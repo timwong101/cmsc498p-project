1374b0ab91db79ec2b19165e1e155c8f35e9fc63
Haitian Hao <harryhao97@gmail.com>
2020-12-23 02:04:38 -0500

    ranked working now

--- gam_package/ranked_medoids.py
+++ gam_package/ranked_medoids.py
@@ -9,7 +9,7 @@ from pandas.plotting import parallel_coordinates
 class RankedMedoids:
     def __init__(self, n_clusters=1, dist_func='euclidean', max_iter=1000, tol=0.0001,
                  f="mushroom-attributions-200-samples.csv"):  # conf = SparkConf().setAppName("project-gam")
-        self.filename = f
+        self.filepath = f
         self.k = n_clusters
 
         self.max_iter = max_iter
@@ -19,30 +19,12 @@ class RankedMedoids:
 
     # Attain the original dataset for the analysis
     def getDataset(self):
-        data = pd.read_csv("mushroom-attributions-200-samples.csv")
-        data = data[['odor', 'bruises']]  # Reduce dims of dataset in order to visualize
+        data = pd.read_csv(self.filepath)
         dataset = []
         l = len(data)
-        # l = 100
         for i in range(l):
             dataset.append(data.loc[i])
         return dataset
-
-    # Generate 2-d test data
-    def generate2dData(self):
-        xs = []
-        ys = []
-        n = 500
-        for i in range(n):
-            xs.append(random.random())
-            ys.append(random.random())
-            if i > .9 * n:
-                xs.append(random.random() + .5)
-                ys.append(random.random() + .5)
-        data = []
-        for i in range(n):
-            data.append([xs[i], ys[i]])
-        return data
 
     # Compute distance between each pair of data points
     # data: the actual data we use for this algorithm, it is a collection of data points, each point has dimension dims
@@ -87,30 +69,28 @@ class RankedMedoids:
         return rank, similarityMetrix
 
     # Compute the hostility value of a data point
-    # tableS: a nxn matrix containing all Sij values
-    # setG: set of integers - indexes of data points
-    # n: size of dataset
-    def getHv(self, index, m, n, rankTable, setG):
-        hv = m * (m + 1) / 2
-        for j in range(n):
-            if not j in setG:
-                for r in rankTable[index]:
-                    if r < rankTable[index][j]:
+    def getHv(self, curr, m, rankTable, inside, outside):
+        hv = m * (m + 1.0) / 2
+        rankList = rankTable[curr]
+        for ins in inside:
+            if ins != curr:  # Need to exempt pairing with itself
+                for outs in outside:
+                    if rankList[outs] < rankList[ins]:
                         hv += 1
         return hv
 
-    # Randomly select k data points as our starting k medoids.
+    # Randomly select k data points as starter k medoids.
     def randomMedoids(self, k, data):
         medoids = set()
         for i in range(k):
             oldLen = len(medoids)
-            while (len(medoids) == oldLen):
+            while len(medoids) == oldLen:
                 medoids.add(random.randint(0, len(data) - 1))
         return medoids
 
     # Assign each points to closest medoid, using Rank table
     def assignToClusters(self, k, n, medoids, rankTable):
-        groups = []  # list of Set
+        groups = []
         for i in range(k):
             groups.append([])  # Make k empty groups
         medoidsList = list(medoids)
@@ -118,28 +98,50 @@ class RankedMedoids:
         for i in range(n):
             rankRowI = rankTable[i]
             min = n
+            groupIndex = n
             for j in range(k):
                 m = medoidsList[j]
                 if rankRowI[m] < min:
                     min = rankRowI[m]
-            for j in range(k):
-                m = medoidsList[j]
-                if rankRowI[m] == min:
-                    groups[j].append(i)
+                    groupIndex = j
+
+            groups[groupIndex].append(i)
 
         return groups, medoids
 
     # Update medoids: use the two tables to calculate the point with highest hv value in each cluster and set it as the new medoid
-    def updateMedoids(self, k, m, n, medoids, similarityTable, rankTable):
+    def updateMedoids(self, m, n, medoids, similarityTable, rankTable):
         newMedoids = set()
         for med in medoids:
-            mostSimilar = similarityTable[med][:m]
+            candidates = []
+            insidePoints = similarityTable[med][:m]
+            outsidePoints = similarityTable[med][m:]
             maxHv = (-1, -1)
-            for simi in mostSimilar:
-                hv = self.getHv(simi, m, n, rankTable, set(mostSimilar))
+            for ins in insidePoints:
+                hv = self.getHv(ins, m, rankTable, set(insidePoints), set(outsidePoints))
+                newHvPair = (ins, hv)
                 if hv > maxHv[1]:
-                    maxHv = (simi, hv)
-            newMedoids.add(maxHv[0])
+                    maxHv = (ins, hv)
+                    candidates.insert(0, (ins, hv))
+                else:
+                    insertion = 0
+                    for i in range(len(candidates)):
+                        if hv > candidates[0][1]:
+                            candidates.insert(insertion, newHvPair)
+                            break
+                        else:
+                            insertion += 1
+
+            if not maxHv[0] in newMedoids:
+                newMedoids.add(maxHv[0])
+            else:
+                pointer = 1
+                while candidates[pointer][0] in newMedoids:
+                    pointer += 1
+                newMedoids.add(candidates[pointer][0])
+
+        print(newMedoids)
+        return newMedoids
 
     def fit(self, X=None, plotit=False, verbose=True):
         """
@@ -172,25 +174,11 @@ class RankedMedoids:
         for i in range(len(clusters)):
             print(clusters[i])
 
-    def testPrintTables(self, data):
-        r, s = self.buildRankTable(data)
-        print("Rank Table: ")
-        self.printNice(r)
-        print()
-        print("Similarity Table: ")
-        self.printNice(s)
-
-    def test2D(self):
-        self.main(self.generate2dData())
-
     # Cluster the data
     def cluster(self):
-        df = pd.read_csv('mushroom-attributions-200-samples.csv')
-        dataset = []
-        for i in range(len(df)):
-            dataset.append(df.loc[i])
+        dataset = self.getDataset()
         medoids, clusters = self.main(dataset)
-        return medoids, clusters, len(df)
+        return medoids, clusters, len(dataset)
 
     # This method checks whether there exists same data points in different clusters
     def checkClustersContainDifferent(self, clusters):
@@ -201,16 +189,6 @@ class RankedMedoids:
                         if d in clusters[j]:
                             return False
         return True
-
-    # Standardize data before the PCA analysis
-    def standardizeData(self, df):
-        features = df.columns
-        # Separating out the features
-        x = df.loc[:, features].values
-        # Separating out the target
-
-        # Standardizing the features
-        x = StandardScaler().fit_transform(x)
 
     # Perform PCA and plot
     def pcaDataAndPlot(self, x, clusters):
@@ -236,11 +214,16 @@ class RankedMedoids:
         n = len(data)
         numOfLoops = 100
         k = 5
-        m = 12
+        m = 15
         medoids = self.randomMedoids(k, data)
         rankTable, similarityMetrix = self.buildRankTable(data)
+        medoidsAttained = set()
         for i in range(numOfLoops):
-            self.updateMedoids(k, m, n, medoids, similarityMetrix, rankTable)
+            medoids = self.updateMedoids(m, n, medoids, similarityMetrix, rankTable)
+            if medoids in medoidsAttained:
+                break
+            else:
+                medoidsAttained.add(frozenset(medoids))
         clusters, medoids = self.assignToClusters(k, n, medoids, rankTable)
 
         self.printNice(clusters)
